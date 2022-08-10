cf450a653d82a4cbd9ea76d032941cdef1523c9c
Ekam Preet Singh <ekampreet.singh6862@gmail.com>
2022-08-10 17:32:00 +0530

    Updated

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import MyCountries from "../Components/MyCountries.js";
+import MyCountries from "../components/MyCountries";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://restcountries.com/v3.1/all");
